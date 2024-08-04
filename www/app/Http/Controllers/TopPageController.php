da9150f2fba4deb893a0b1bc08de0e01ac953ead
yamada <naoki19980422@gmail.com>
2024-08-04 23:04:44 +0900

    Dockerfile変更

--- www/app/Http/Controllers/TopPageController.php
+++ www/app/Http/Controllers/TopPageController.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class TopPageController extends Controller
 {
-    //
+    public function init(): View
+    {
+        return view('TopPage');
+    }
 }
