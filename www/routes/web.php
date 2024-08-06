dc0c29a87a8fde2bc77d3bce9e00f1e8256a34b6
yamada <naoki19980422@gmail.com>
2024-08-06 23:01:48 +0900

    dashboard作成

--- www/routes/web.php
+++ www/routes/web.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\TopPageController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/top', [TopPageController::class, 'init'])->name('top.init');
+Route::post('/top/add', [TopPageController::class, 'add'])->name('top.add');
